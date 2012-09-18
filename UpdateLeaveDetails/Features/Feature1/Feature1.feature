25d665840e273b76154cb7c873494bb97805e972
unknown <arockiaraj@TILLID-PC1.caldera.com>
2012-09-18 18:09:42 +0530

    my initial upload

---
+++ UpdateLeaveDetails/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="74bbf7c9-8eb6-4b0c-8346-6644d27f069b" featureId="74bbf7c9-8eb6-4b0c-8346-6644d27f069b" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="UpdateLeaveDetails Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="d2c62542-b6e1-4c91-a142-8362aae23ce1" />
+  </projectItems>
+</feature>
---
+++ UpdateLeaveDetails/UpdateLeaveDetail/UpdateLeaveDetail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System.Data;
+
+namespace UpdateLeaveDetails.UpdateLeaveDetail
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class UpdateLeaveDetail : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item was updated.
+        /// </summary>
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+           
+                SPListItem item = properties.ListItem;
+                var createdByLeaveRequest = new SPFieldUserValue(properties.Web,item["Created By"].ToString());
+                
+                if (item["Status"].ToString() == "Approved")
+
+                {
+                    SPSite myDestinationSite = new SPSite(properties.WebUrl);
+                    SPWeb myDestinationWeb = myDestinationSite.OpenWeb();
+                    SPList myDestinationList = myDestinationWeb.Lists["LeaveDays"];
+                    SPListItem myDestinationListItem = myDestinationList.Items[0];                
+                    
+                    var EmpNameLeavedays = new SPFieldUserValue(properties.Web, myDestinationListItem["Employee Name"].ToString());
+                    myDestinationListItem["PaidLeaveBalance"] = item["Dup Paid Leave Balance"];//SourceEmpId;
+                    myDestinationListItem["PaidLeaveUtilized"] = item["Dup Paidleaveutilize"];// SourceEmpName;
+                    myDestinationWeb.AllowUnsafeUpdates = true;
+                    if(createdByLeaveRequest.User.ID  == EmpNameLeavedays.User.ID)
+                   {
+                    myDestinationListItem.Update();
+                    myDestinationWeb.AllowUnsafeUpdates = false;
+                   }
+
+
+                }
+
+            
+        }
+
+
+    }
+}
